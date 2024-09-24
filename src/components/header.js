e7bbb989dc852141b4eb9b9956f400829bea73d4
GainorJoshua-FS <JQGainor@student.fullsail.edu>
2024-09-24 02:36:41 -0400

    Some cleanup and header started

---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <header style={styles.header}>
+            Lorem
+        </header>
+    )
+}
+
+export default Header
+
+const styles ={
+    header:{
+    },
+}
