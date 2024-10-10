b33c428c34aec158d1bbae2337a7ff38609fd8ac
GainorJoshua-FS <JQGainor@student.fullsail.edu>
2024-10-10 03:24:43 -0400

    Changes to footer and header. Also changed favicon

--- src/components/header.js
+++ src/components/header.js
@@ -6,13 +6,14 @@ import { Link } from 'react-router-dom'
 function Header() {
     return (
         <header className='PrimBack' style={styles.header}>
-            <Link id="link" to="/" style={styles.link}>
-                <img style={styles.logo} src={Logo2} alt=''/>
+            
+            <Link className='LinkLogo' to="/" style={styles.link}>
+                <img className='Logo' src={Logo2} alt=''/>
             </Link>
-            <Link id="link" to="/" style={styles.link}>
+            <Link className='Link' id="link" to="/" style={styles.link}>
                 Home
             </Link>
-            <Link id="link" to="/contact" style={styles.link}>
+            <Link className='Link' id="link" to="/contact" style={styles.link}>
                 Contact Us
             </Link>
         </header>
@@ -22,24 +23,17 @@ function Header() {
 export default Header
 
 const styles ={
-    header:{
-        // backgroundColor: '#000045',
-        // backgroundColor: '#2f5065',
-        // padding: '20px 0px',
-        color: "#9abbd0"
-    },
-    logo:{
-        width: '10%',
-        height: '10%'
-    },
-    link:{
-        color: "white",
-        textDecoration: "none",
-        display: "inline",
-        textAlign: "left",
-        marginRight: '20px'
-    },
-    div:{
-        color:'#C3AD54'
-    }
+    // header:{
+    //     color: "#9abbd0"
+    // },
+    // link:{
+    //     color: "white",
+    //     textDecoration: "none",
+    //     display: "inline",
+    //     textAlign: "left",
+    //     marginRight: '20px'
+    // },
+    // div:{
+    //     color:'#C3AD54'
+    // }
 }
