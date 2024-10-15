3e54b6a88a73f9f17e822e852481103054103385
GainorJoshua-FS <JQGainor@student.fullsail.edu>
2024-10-14 23:26:23 -0400

    Header changes made

--- src/components/header.js
+++ src/components/header.js
@@ -5,17 +5,22 @@ import { Link } from 'react-router-dom'
 
 function Header() {
     return (
-        <header className='PrimBack' style={styles.header}>
-            {/* <h1>Weber Tax &amp; Accounting</h1> */}
-            <Link className='LinkLogo' to="/" style={styles.link}>
-                <img className='Logo' src={Logo2} alt=''/>
-            </Link>
-            <Link className='Link' id="link" to="/" style={styles.link}>
-                Home
-            </Link>
-            <Link className='Link' id="link" to="/contact" style={styles.link}>
-                Contact Us
-            </Link>
+        <header className='PrimBack'>
+            <article>
+                <Link className='LinkLogo' to="/" style={styles.link}>
+                    <img className='Logo' src={Logo2} alt=''/>
+                </Link>
+                <h1 className='Tert'>Weber <br/> Tax &amp; Accounting</h1>
+            </article>
+
+            <div>
+                <Link className='Link' id="link" to="/" style={styles.link}>
+                    Home
+                </Link>
+                <Link className='Link' id="link" to="/contact" style={styles.link}>
+                    Contact Us
+                </Link>
+            </div>
         </header>
     )
 }
